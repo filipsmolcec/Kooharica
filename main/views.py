b9ba47d2ff3478645c8399a7c711f524d4271165
DESKTOP-6VT07P5\Filip <snakegamer13@gmail.com>
2024-12-05 19:21:01 +0100

    Add user auth, add simple login and register page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def index(request):
+    return render(request, "main/index.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
