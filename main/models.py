945847d5666f72fc6ea87e6dbc662e1df38bdd2d
DESKTOP-6VT07P5\Filip <snakegamer13@gmail.com>
2024-12-06 12:37:38 +0100

    Add BlogPost model, remove User model, fix Admin Panel button href pointing to wrong url

--- main/models.py
+++ main/models.py
@@ -1,18 +1,20 @@
 from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return "{} {}".format(self.first_name, self.last_name)
-    
+        return self.title
+
 class Recipe(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=255)
     instructions = models.TextField(blank=False, null=False)
     cook_time_minutes = models.IntegerField()
     author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
@@ -24,11 +26,11 @@ class Recipe(models.Model):
     
 class Review(models.Model):
     recipe = models.ForeignKey(Recipe, default=1, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     comment = models.TextField(blank=True, null=True)
     date_posted = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return "{}Review{}".format(str(self.user), str(self.date_posted))
+        return "{}Review{}".format(str(self.author), str(self.date_posted))
     
