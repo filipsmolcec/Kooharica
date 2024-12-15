c5909eda5d027fad1a1663cd1977019124e5dfc7
Nika <nika.bolic@gmail.com>
2024-12-15 14:44:15 -0800

    Add recipe and blog post ListViews

--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
+from .models import *
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+class AllRecipes(ListView):
+    model = Recipe
+    template_name = "main/all_recipes.html"
+
+class AllBlogPosts(ListView):
+    model = BlogPost
+    template_name = "main/all_blog_posts.html"
+
+class RecipeDetail(DetailView):
+    model = Recipe
 
 def index(request):
     return render(request, "main/index.html")
