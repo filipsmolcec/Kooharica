40ceedf80f47a2e5094f222d8e33cf9c337b1eeb
Filip <snakegamer13@gmail.com>
2024-12-16 10:30:20 +0100

    Replace hardcoded URL references to Django name reference

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recepti/', views.AllRecipes.as_view()),
-    path('recepti/<pk>/', views.RecipeDetail.as_view()),
-    path('blogovi/', views.AllBlogPosts.as_view()),
-    path('blogovi/<pk>/', views.BlogDetail.as_view()),
-    path('korisnici/<pk>/', views.UserDetail.as_view()),
+    path('recepti/', views.AllRecipes.as_view(), name='recipes'),
+    path('recepti/<pk>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('blogovi/', views.AllBlogPosts.as_view(), name='blog_posts'),
+    path('blogovi/<pk>/', views.BlogDetail.as_view(), name='blog_detail'),
+    path('korisnici/<pk>/', views.UserDetail.as_view(), name='user_detail'),
     path('accounts/register/', views.register, name='register')
 ]
